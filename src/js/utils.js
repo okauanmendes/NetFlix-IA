d1a5005c5a2bc04a97e7f38dd4183d755a7f87e4
Kauan Mendes <kauanmendesivo180@gmail.com>
2026-04-02 18:06:28 -0300

    feat(utils): adiciona função para extrair ID do YouTube

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,1 +1,7 @@
-
+export function getYouTubeId(url) {
+    if (!url) return "7RUA0IOfar8";
+    if (url.includes('v=')) {
+        return url.split('v=')[1].split('&')[0];
+    }
+    return url.split('/').pop();
+}
