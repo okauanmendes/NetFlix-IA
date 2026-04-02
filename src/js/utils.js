e9f8c1bba4f63e3f17d5da23a199152d06155d87
Kauan Mendes <kauanmendesivo180@gmail.com>
2026-04-02 18:13:36 -0300

    feat(utils): adiciona função para gerar duração aleatória e adiciona função para classificação indicativa aleatória

--- src/js/utils.js
+++ src/js/utils.js
@@ -9,3 +9,11 @@ export function getYouTubeId(url) {
 export function getRandomMatchScore() {
     return Math.floor(Math.random() * 20 + 80);
 }
+
+export function getRandomDuration(hasProgress) {
+    return hasProgress ? '10 temporadas' : '2h ' + Math.floor(Math.random() * 59) + 'm';
+}
+
+export function getRandomAgeBadge() {
+    return Math.random() > 0.5 ? { text: 'A16', class: 'red-accent' } : { text: '16', class: '' };
+}
